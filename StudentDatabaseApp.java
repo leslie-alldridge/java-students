700b877c82d7e6b480afba1f8795c9cb1685a409
leslie.alldridge <leslie.alldridge@gmail.com>
2019-02-19 06:19:36 +1300

    added arrays

--- StudentDatabaseApp.java
+++ StudentDatabaseApp.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 /**
  * Main program
  *
@@ -15,9 +15,16 @@ public class StudentDatabaseApp
      */
     public static void main (String[] args)
     {
-        Student stu1 = new Student();
-        stu1.enroll();
-        stu1.payTuition();
-        System.out.println(stu1.toString());
+        System.out.print("Enter number of new students to enroll: ");
+        Scanner in = new Scanner(System.in);
+        int numOfStudents = in.nextInt();
+        Student[] students = new Student[numOfStudents];
+        
+        for (int n = 0; n < numOfStudents; n++) {
+        students[n] = new Student();
+        students[n].enroll();
+        students[n].payTuition();
+        System.out.println(students[n].toString());
     }
+   }   
 }
